ee4c5ff50d71665d47940556ee94a6af193fc568
Oladiy <bd3396121@gmail.com>
2020-12-15 15:18:31 +0300

    [ADD] Get total supply and get balance interactions with contract

--- src/interactWithContract.js
+++ src/interactWithContract.js
@@ -1,5 +1,3 @@
-const detectEthereumProvider = require('@metamask/detect-provider');
-const ethers = require('ethers');
 const express = require('express');
 const ContractMeta = require('./ContractMeta');
 const Web3 = require('web3');
@@ -16,15 +14,7 @@ const contractAddress = ContractMeta.Address;
 const ABI = ContractMeta.ABI;
 
 const token = new web3.eth.Contract(ABI, contractAddress);
-
 const myAddress = '0x277071ADA51DF484D2f87Ee02D559453A39ae5ca';
-const receiverAddress = '0x6290C445A720E8E77dd8527694030028D1762073';
-
-let senderAddress = '0x0';
-let tokenContract = null;
-let provider;
-let signer;
-
 
 app.get('/', async (request, response) => {
     response.sendFile(__dirname + '/templates/' + 'index.html');
@@ -59,40 +49,6 @@ app.get('/get_total_supply/', async (request, response) => {
 
     response.send(`The total supply is ${totalSupply}.`);
 });
-
-app.post('/transfer/', async (request, response) => {
-    const donationSum = 1337;
-
-    provider = await detectEthereumProvider();
-    signer = provider.getSigner();
-
-    if (!provider) {
-        console.log('Please install MetaMask!');
-        return;
-    }
-
-    loadWallet();
-    getTokenContract();
-    await console.log('.....');
-
-    await tokenContract.functions.transfer(donationSum, {
-        from: senderAddress,
-        gasLimit: 500000,
-    });
-
-    response.send(`Successfully donated 0.${donationSum} ETH.`)
-});
-
-function loadWallet(){
-    signer.getAddress().then(async (response) => {
-        senderAddress = response;
-        await console.log(senderAddress);
-    });
-}
-
-function getTokenContract(){
-    tokenContract = new ethers.Contract(contractAddress, ABI, signer);
-}
 
 const server = app.listen(5002, () => {
     const port = server.address().port;
